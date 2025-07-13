7c14c935b2ecfdf8ecaea5d374fd11a9fb616992
beganime <begenchyagmurow2008@gmail.com>
2025-07-13 11:33:09 +0500

    Update settings.py

--- mafia/settings.py
+++ mafia/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
+import os
+os.environ['TZ'] = ':/usr/share/zoneinfo/posix/UTC'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,7 +86,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
